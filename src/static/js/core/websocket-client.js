ce7b8691b8d3568026875302ccd0e6a5751d1349
zhuyueming2012 <zhuyuemingtg@outlook.com>
2025-01-09 22:43:25 +0800

    Update websocket-client.js

--- src/static/js/core/websocket-client.js
+++ src/static/js/core/websocket-client.js
@@ -154,11 +154,11 @@ export class MultimodalLiveClient extends EventEmitter {
             this.emit('toolcallcancellation', response.toolCallCancellation);
             return;
         }
-        if (response.setupComplete) {
+        /*if (response.setupComplete) {
             this.log('server.send', 'setupComplete');
             this.emit('setupcomplete');
             return;
-        }
+        }*/
         if (response.serverContent) {
             const { serverContent } = response;
             if (serverContent.interrupted) {
@@ -166,10 +166,10 @@ export class MultimodalLiveClient extends EventEmitter {
                 this.emit('interrupted');
                 return;
             }
-            /*if (serverContent.turnComplete) {
+            if (serverContent.turnComplete) {
                 this.log('server.send', 'turnComplete');
                 this.emit('turncomplete');
-            }*/
+            }
             if (serverContent.modelTurn) {
                 let parts = serverContent.modelTurn.parts;
                 const audioParts = parts.filter((p) => p.inlineData && p.inlineData.mimeType.startsWith('audio/pcm'));
