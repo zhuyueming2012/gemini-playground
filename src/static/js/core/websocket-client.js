8f3d18e8edb032358c4b87db7db82b624a3bb402
zhuyueming2012 <zhuyuemingtg@outlook.com>
2025-01-09 22:24:28 +0800

    Update websocket-client.js

--- src/static/js/core/websocket-client.js
+++ src/static/js/core/websocket-client.js
@@ -190,8 +190,8 @@ export class MultimodalLiveClient extends EventEmitter {
 
                 parts = otherParts;
                 const content = { modelTurn: { parts } };
-                this.emit('content', content);
-                this.log(`server.content`, response);
+                //this.emit('content', content);
+                //this.log(`server.content`, response);
             }
         } else {
             console.log('Received unmatched message', response);
